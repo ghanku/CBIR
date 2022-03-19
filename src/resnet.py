17f363568ca3fef4aeb24b12287c8383dbf4e22b
ghanku <chabiraghanou45@gmail.com>
2022-03-19 23:25:30 +0100

    Update resnet.py
    changed depricated old functions

--- src/resnet.py
+++ src/resnet.py
@@ -11,7 +11,7 @@ import torch.utils.model_zoo as model_zoo
 
 from six.moves import cPickle
 import numpy as np
-import scipy.misc
+import imageio
 import os
 
 from evaluate import evaluate_class
@@ -140,7 +140,7 @@ class ResNetFeat(object):
       data = db.get_data()
       for d in data.itertuples():
         d_img, d_cls = getattr(d, "img"), getattr(d, "cls")
-        img = scipy.misc.imread(d_img, mode="RGB")
+        img = imageio.imread(input, pilmode='RGB')
         img = img[:, :, ::-1]  # switch to BGR
         img = np.transpose(img, (2, 0, 1)) / 255.
         img[0] -= means[0]  # reduce B's mean
