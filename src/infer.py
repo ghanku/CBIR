cd6e95a31d9d2a28874fe1a8f9acd61c808767a3
ghanku <chabiraghanou45@gmail.com>
2022-05-26 12:12:05 +0100

    fix

--- src/infer.py
+++ src/infer.py
@@ -33,62 +33,50 @@ if (len(sys.argv) > 1):
     query_idx = int(sys.argv[3])
     feature_type = sys.argv[4]
 
-# Feature_type = "daisy"   # daisy descriptor
-# Feature_type = "edge"   # filtering (edge detection)
-#Feature_type = "gabor"
-# Feature_type = "HOG"    # histogram of gradients
-# Feature_type = "ccv2"  # color coherence vector
-# Feature_type = "dcd"   # dominant color descriptor
-# Feature_type = "efd"   # elleptical fourier descriptor
-# Feature_type = "gvf"   # gradient vector flow
-# Feature_type = "lbp"   # local binary pattern
-#Feature_type = "slantlet"
-#Feature_type = "wavelet"
-#Feature_type = "fusion"
 
 if __name__ == '__main__':
     db = Database()
 
-    if (feature_type="color"):
+    if (feature_type == "color"):
         # retrieve by color
         method = Color()
-    elif(feature_type="daisy"):
+    elif(feature_type == "daisy"):
         # retrieve by daisy
         method = Daisy()
-    elif(feature_type="edge"):
+    elif(feature_type == "edge"):
         # retrieve by edge
         method = Edge()
-    elif(feature_type="gabor"):
+    elif(feature_type == "gabor"):
         # retrieve by gabor
         method = Gabor()
-    elif(feature_type="HOG"):
+    elif(feature_type == "HOG"):
         # retrieve by HOG
         method = HOG()
-    elif(feature_type="vggnet"):
+    elif(feature_type == "vggnet"):
         # retrieve by VGG
         method = VGGNetFeat()
-    elif(feature_type="resnet"):
+    elif(feature_type == "resnet"):
         # retrieve by resnet
         method = ResNetFeat()
-    elif(feature_type="ccv2"):
+    elif(feature_type == "ccv2"):
         # retrieve by ccv
         method = CCV()
-    elif(feature_type="dcd"):
+    elif(feature_type == "dcd"):
         # retrieve by dcd
         method = DCD()
-    elif(feature_type="efd"):
+    elif(feature_type == "efd"):
         # retrieve by efd
         method = EFD()
-    elif(feature_type="gvf"):
+    elif(feature_type == "gvf"):
         # retrieve by gvf
         method = GVF()
-    elif(feature_type="lbp"):
+    elif(feature_type == "lbp"):
         # retrieve by lbp
         method = LBP()
-    elif(feature_type="slantlet"):
+    elif(feature_type == "slantlet"):
         # retrieve by slantlet
         method = SLANT()
-    elif(feature_type="wavelet"):
+    elif(feature_type == "wavelet"):
         # retrieve by wavelet
         method = WAVELET()
 
